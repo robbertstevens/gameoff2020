7ce629b694d8ea4aa9302de396c78ec1dab32903
WemkoDijkhuis <wemko.dijkhuis@trivento.nl>
2020-11-03 20:26:21 +0100

    ui

--- black_hole/black_hole.gd
+++ black_hole/black_hole.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-signal ball_in
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,7 +7,7 @@ signal ball_in
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	emit_signal("ball_in")
+
 	pass # Replace with function body.
 
 
