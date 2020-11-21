c465c5ebf56763a0e9604a88f0a8ffb88c6d2687
Robbert Stevens <robbertstevens@gmail.com>
2020-11-21 11:24:48 +0100

    Progress & move some things

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,11 +3,14 @@ extends KinematicBody2D
 onready var AnimationTree = $AnimationTree
 onready var AnimationState = AnimationTree.get("parameters/playback")
 
-const MAX_SPEED: int = 80;
-const ACCELERATION: int = 500
-const FRICTION: int = 500;
+export var MAX_SPEED: int = 80;
+export var ACCELERATION: int = 500
+export var FRICTION: int = 500;
 
 var velocity: Vector2 = Vector2.ZERO;
+
+func _ready():
+	pass
 
 func _physics_process(delta) -> void:
 	var axis = get_input_axis()
@@ -19,11 +22,11 @@ func _physics_process(delta) -> void:
 		
 		velocity = velocity.move_toward(axis * MAX_SPEED, ACCELERATION * delta)
 	else:
+
 		AnimationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
-	
 
 func get_input_axis() -> Vector2:
 	var axis = Vector2.ZERO
